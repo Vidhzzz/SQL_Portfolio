564ae2fc86b5539b118718bc66544e8573956229
Vidhzzz <140083581+Vidhzzz@users.noreply.github.com>
2026-08-04 20:31:30 +0530

    Add SQL queries using subqueries for employee data

--- subqueries.sql
+++ subqueries.sql
@@ -1,1 +1,33 @@
+SELECT *
+FROM Employee
+WHERE salary>
+(
+SELECT AVG(salary)
+FROM Employee
+);
 
+SELECT *
+FROM Employee
+WHERE salary=
+(
+SELECT MAX(salary)
+FROM Employee
+);
+
+SELECT emp_name
+FROM Employee
+WHERE dept_id=
+(
+SELECT dept_id
+FROM Department
+WHERE dept_name='IT'
+);
+
+SELECT *
+FROM Employee
+WHERE dept_id IN
+(
+SELECT dept_id
+FROM Department
+WHERE dept_name IN ('IT','Finance')
+);
