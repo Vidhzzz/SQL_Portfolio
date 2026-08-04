eb6aeaf666718722c871089a2b3d4c1e03130e73
Vidhzzz <140083581+Vidhzzz@users.noreply.github.com>
2026-08-04 20:30:41 +0530

    Add multiple SQL queries for Employee table

--- basic_queries.sql
+++ basic_queries.sql
@@ -1,1 +1,26 @@
+SELECT * FROM Employee;
 
+SELECT emp_name,salary FROM Employee;
+
+SELECT DISTINCT dept_id FROM Employee;
+
+SELECT * FROM Employee
+WHERE salary>60000;
+
+SELECT * FROM Employee
+WHERE gender='Female';
+
+SELECT * FROM Employee
+ORDER BY salary DESC;
+
+SELECT * FROM Employee
+ORDER BY emp_name;
+
+SELECT * FROM Employee
+LIMIT 5;
+
+SELECT * FROM Employee
+WHERE age BETWEEN 24 AND 28;
+
+SELECT * FROM Employee
+WHERE emp_name LIKE 'R%';
