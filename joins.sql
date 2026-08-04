2337bd8d67612dee568f7f8951f06e38d71cea29
Vidhzzz <140083581+Vidhzzz@users.noreply.github.com>
2026-08-04 20:31:06 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -1,1 +1,19 @@
+SELECT e.emp_name,d.dept_name
+FROM Employee e
+JOIN Department d
+ON e.dept_id=d.dept_id;
 
+SELECT *
+FROM Employee e
+LEFT JOIN Department d
+ON e.dept_id=d.dept_id;
+
+SELECT *
+FROM Employee e
+RIGHT JOIN Department d
+ON e.dept_id=d.dept_id;
+
+SELECT e.emp_name,m.emp_name AS manager
+FROM Employee e
+LEFT JOIN Employee m
+ON e.manager_id=m.emp_id;
